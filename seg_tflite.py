3e469e7c33467c75d058ef77cc1164336d5d4631
Jatindra Suthar <sutharjatindra@gmail.com>
2023-04-07 10:12:54 +0530

    Update seg_tflite.py

--- seg_tflite.py
+++ seg_tflite.py
@@ -2,15 +2,9 @@ import numpy as np
 import tensorflow as tf
 import cv2
 
-# https://www.tensorflow.org/lite/examples/object_detection/overview
-# https://github.com/tensorflow/tensorflow/issues/51591
-# https://pypi.org/project/googlenet-pytorch/
-
-#model_path = "./object_detection_mobile_object_localizer_v1_1_default_1.tflite"
-model_path = "static/models/20093fc4-4c4a-4976-9289-b81151d1e3ee.tflite"
-#LABELS = ['Hartford', 'Zinfandel', 'Chambolle Musigny', 'Les Fuees', 'Carlisle']
-LABELS = ['clean', 'dirty']
-image_path= '../static/images/from_web.png'
+model_path = ""
+LABELS = []
+image_path = "
 THRESHOLD = 0.5
 COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype=np.uint8)
 
@@ -33,8 +27,6 @@ interpreter.set_tensor(input_details[0]['index'], [processed_img])
 interpreter.invoke()
 
 
-
-
 def get_output_tensor(interpreter, index):
     output_details = interpreter.get_output_details()[index]
     tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
@@ -51,8 +43,8 @@ def draw_rect(results, image):
 
         class_id = int(obj['class_id'])
         label = "{}: {:.0f}%".format(LABELS[class_id], obj['score'] * 100)
-        # color = [int(c) for c in COLORS[class_id]]
-        color = (0, 0, 0)
+        color = [int(c) for c in COLORS[class_id]]
+
         #y = y_min - 15 if y_min - 15 > 15 else y_min + 15
 
         cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (13, 13, 13), 2)
@@ -77,7 +69,7 @@ print('count ==>', count)
 results = []
 for i in range(count):
     if classes[i] >= THRESHOLD:
-        result = {'bounding_box': rects[i], 'class_id': scores[i], 'score': classes[i] }
+        result = {'bounding_box': rects[i], 'score': scores[i], 'class_id': classes[i] }
         results.append(result)
 print()
 print('results ==>', results)
